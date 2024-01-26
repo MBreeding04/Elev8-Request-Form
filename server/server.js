a6e456f9f5c2d8fcc6ca7dcd5d67a3b94140a933
Matthew <03mattman@gmail.com>
2024-01-26 13:54:42 -0500

    working shell and prototype, takes input from form, submit to database and github issues, visualizes data

--- server/server.js
+++ server/server.js
@@ -10,11 +10,11 @@ app.use(cors())
 app.use(express.raw({ type: 'application/octet-stream', limit: '5mb' }));
 app.use(express.json({ limit: '5mb' })); // Add other middleware as needed
 const db = mysql.createConnection({
-    host: process.env.REACT_APP_HOST,
-    user: process.env.REACT_APP_USER,
-    password: process.env.REACT_APP_PASSWORD,
-    database: process.env.REACT_APP_DATABASE,
-    port: process.env.REACT_APP_PORT
+    host: 'sql9.freemysqlhosting.net',
+    user: 'sql9678711',
+    password: 'dUyhpX35jf',
+    database: 'sql9678711',
+    port: '3306'
 })
 db.connect((err) => {
     if (err) {
